e915eb949473ff7f26ccf04d9d508dfbed4cc7f2
Matthew Sain <matthews@matthews-mbp.local>
2012-09-03 12:48:27 -0700

    Init

---
+++ fetch.js
@@ -0,0 +1,199 @@
+var domino = require('domino');
+var fs = require('fs');
+var request = require('request');
+var email   = require("emailjs");
+var Handlebars = require('handlebars');
+var redis = require("redis"),
+    client = redis.createClient();
+
+var tpl = fs.readFileSync('./place.stache', 'utf8');
+var lay = fs.readFileSync('./email_layout.stache', 'utf8');
+
+var template = Handlebars.compile(tpl);
+var layout = Handlebars.compile(lay);
+
+var query = 'http://sfbay.craigslist.org/search/apa/sfc?hasPic=1&maxAsk=3200&minAsk=2000&nh=10&nh=11&nh=12&nh=149&nh=18&nh=21&nh=4&srchType=T';
+
+var sendEmail = 1;
+  
+var server  = email.server.connect({
+   user:    'mattsain@gmail.com', 
+   password:"mangLezo84", 
+   host:    "smtp.gmail.com", 
+   ssl:     true
+});
+
+var message = {
+   text:    "testing", 
+   from:    "craigslist update <mattsain@gmail.com>", 
+   to:      "munchkin <sunita.bose@gmail.com>, me <mattsain@gmail.com>"
+};
+
+var places = {};
+var msgs = [];
+
+
+//DB
+client.on("error", function (err) {
+  console.log("Error " + err);
+});
+
+function getPlaceId(href) {
+  return href.substring(href.lastIndexOf('/') + 1, href.indexOf('.html'));
+}
+
+function addPlace(obj) {
+  var uid = getPlaceId(obj.href);
+  client.HMSET(uid, obj);
+  client.SADD('places', uid);
+  places[uid] = obj;
+}
+
+function getPlaces(cb) {
+  var loaded = -1;
+  var tot = 0;
+  
+  function addLoaded() {
+    loaded += 1;
+    if (loaded >= tot) {
+      cb(places);
+    }
+  } 
+
+  client.SMEMBERS('places', function (err, replies) {
+
+    tot = replies.length;
+    addLoaded();
+
+    replies.forEach(function(uid) {
+      if (places[uid]) addLoaded();
+      client.hgetall(uid, function (err, obj) {
+        places[uid] = obj;
+        addLoaded();
+      });
+    });
+  });
+}
+
+
+function scrape(index, cb) {
+  request(index, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var win = domino.createWindow(body);
+      var doc = win.document;
+
+      cb(body, win, doc);
+    } else {
+      console.log(index, 'error');
+    }
+  });
+}
+
+
+function isUnique(href) {
+  var uid = getPlaceId(href);
+  return !places[uid];
+}
+
+
+function stripText(txt) {
+  var hasImgs = txt.indexOf('<!--\nimgList');
+  return (hasImgs < 0) ? txt : txt.substring(0, hasImgs);
+}
+
+function placeLoaded() {
+  loadedCount += 1;
+  if (loadedCount === total) {
+    sendmail();
+  }
+}
+
+function sws(txt) {
+  return txt.replace(/\t/g, '').replace(/\n/g, '');
+}
+
+function sendmail() {
+  var body = { body : msgs.join('').replace(/--+/g, ' ') };
+  message.subject = 'Craigslist update ' + (new Date()).toLocaleTimeString();
+  message.attachment = [{
+    data : sws(layout(body)),
+    alternative: true
+  }];
+  if (sendEmail) {
+    server.send(message, function(err, message) { 
+      if (err) console.log(err); 
+    });
+  } else {
+    console.log(message);
+  }
+}
+
+
+function scrapePage(p) {
+
+  scrape(p, function($, win, doc) {
+    var title = doc.querySelector('h2').textContent;
+
+    if (title !== 'This posting has been deleted by its author.') {
+      var place = {
+        title : title,
+        href : p,
+        photos : [],
+        text: stripText(doc.getElementById('userbody').textContent),
+      };
+
+      place.summary = place.text.substring(0, 600);
+
+      var imgset = doc.querySelectorAll('#iwt a');
+      if (imgset.length) {
+        imgset.forEach(function(item) {
+          place.photos.push(item.href);
+        });
+      } else {
+        doc.querySelectorAll('#userbody img').forEach(function(item) {
+          if (item.src.search('facebook|twitter|tweet|linkedin|yelp|feed|rss|created_at|apply_now|header|top|contact_us|footer|logo|common|acctPhoto') < 0) {
+            place.photos.push(item.src);
+          } else {
+            console.log(item.src, 'rejected');
+          }
+        });
+      }
+
+      var maps = doc.querySelector('#leaflet');
+      if (maps) {
+        place.map = {
+          long : maps.getAttribute('data-longitude'),
+          lat : maps.getAttribute('data-latitude')
+        };
+      }
+
+      // add place to db
+      addPlace(place);
+
+      // add place to message
+      msgs.push(template(place));
+    }
+
+    return placeLoaded();
+
+  });
+
+};
+
+
+//initialisation
+var loadedCount = 0;
+var total = 0;
+
+getPlaces(function () {
+  scrape(query, function($, win, doc) {
+    doc.querySelectorAll('p.row a').forEach(function(item, i) {
+      var href = item.href;
+      if (isUnique(href)) {
+        total += 1;
+        scrapePage(href);
+      }
+    });
+  })
+});
+
