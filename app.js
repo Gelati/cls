15a9fab09581945d82ce388dc49014edb244325c
Matt Sain <matt@insaindesign.com>
2012-09-09 12:52:04 -0700

    removing send mail option

--- app.js
+++ app.js
@@ -14,15 +14,13 @@ if (process.env.REDISTOGO_URL) {
     client = redis.createClient();
 }
 
-
 var tpl = fs.readFileSync(__dirname + '/views/place.stache', 'utf8');
 var template = Handlebars.compile(tpl);
 
-var places = {};
-
-function getPlaces(cb) {
+function getPlaces(type, cb) {
   var loaded = -1;
   var tot = 0;
+  var places = {};
 
   function addLoaded() {
     loaded += 1;
@@ -31,7 +29,7 @@ function getPlaces(cb) {
     }
   }
 
-  client.SMEMBERS('places', function (err, replies) {
+  client.SMEMBERS(type, function (err, replies) {
 
     tot = replies.length;
     addLoaded();
@@ -39,7 +37,9 @@ function getPlaces(cb) {
     replies.forEach(function(uid) {
       if (places[uid]) addLoaded();
       client.hgetall(uid, function (err, obj) {
+	if (obj.added) obj.added = (new Date(obj.added * 1)).toDateString();
         obj.photos = (obj.photos) ? obj.photos.split(',') : [];
+	obj.id = uid;
         places[uid] = obj;
         addLoaded();
       });
@@ -60,8 +60,6 @@ function sortit(data) {
   });
 }
 
-
-
 var app = exp();
 /*
 app.set('view engine', 'stache');
@@ -69,8 +67,10 @@ app.engine('stache', require('hbs').__express);
 app.set('views', __dirname + '/views');
 app.use(exp.static(__dirname + '/public'));
 */
+
+
 app.get('/', function(req, res){
-  getPlaces(function (data) {
+  getPlaces('new', function (data) {
     var out = [], page = '', places = sortit(data);
     places.forEach(function(place) {
       out.push(template(place));
@@ -81,8 +81,34 @@ app.get('/', function(req, res){
   });
 });
 
-app.post('/remove', function(req, res) {
-  console.log(arguments);
+app.get('/saved', function(req, res){
+  getPlaces('saved', function (data) {
+    var out = [], page = '', places = sortit(data);
+    places.forEach(function(place) {
+      out.push(template(place));
+    });
+    page = '<h1>' + out.length + ' places saved</h1>';
+    page += out.join('');
+    res.send(page);
+  });
+});
+
+app.get('/remove/:id', function(req, res) {
+  client.SREM('new', req.param('id'), function (err, resp) {
+    res.json({ id : req.param('id'), error : err, resp : resp });
+  });
+});
+
+app.get('/unsave/:id', function(req, res) {
+  client.SREM('saved', req.param('id'), function (err, resp) {
+    res.json({ id : req.param('id'), error : err, resp : resp });
+  });
+});
+
+app.get('/save/:id', function(req, res) {
+  client.SADD('saved', req.param('id'), function (err, resp) {
+    res.json({ id : req.param('id'), error : err, resp : resp });
+  });
 });
 
 app.listen(process.env.PORT);
