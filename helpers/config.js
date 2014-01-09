c2f9ff0f2e822025211ac74bf5453b5842f19f2c
Matt Sain <matt@insaindesign.com>
2014-01-08 21:35:08 -0800

    Removed my email from example

--- helpers/config.js
+++ helpers/config.js
@@ -4,11 +4,16 @@ module.exports = {
   query: 'search/apa/sfc?&maxAsk=3250&minAsk=2000&nh=10&nh=11&nh=12&nh=149&nh=18&nh=21&nh=4&srchType=T',
 
   // e.g., ['email@mail.com'] or for multiple ['email@mail.com', 'email2@mail.com']
-  emails: ['mattsain@hotmail.com'],
+  emails: [''],
 
+  // for local use, either create an `.env` file with key=value on each line. e.g.,
+  // SENDGRID_USERNAME=xxxxxxxxx
+  // SENDGRID_PASSWORD=xxxxxxxxx
+  //
+  // Or edit these values `pass: 'xxxxxxx',`
   sendgrid: {
-    user: process.env.SENDGRID_USERNAME,
     pass: process.env.SENDGRID_PASSWORD,
+    user: process.env.SENDGRID_USERNAME,
   },
 
   /**
